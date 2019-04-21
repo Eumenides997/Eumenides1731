ba1d1611b005453d5cf986c24082e1754c76ec19
Eumenides997 <646224348@qq.com>
2019-04-21 17:10:21 +0800

    异常处理习题 张祯强 3171104131 第二次上传

--- src/异常处理习题/text.java
+++ src/异常处理习题/text.java
@@ -7,7 +7,9 @@ public class text {
 		int f=0;
 		for(int i=0;i<s.length();i++) {
 			if(s.charAt(i)>='0'&&s.charAt(i)<='9') {
-				f=f*10+f-'0';
+				f=f*10+(s.charAt(i)-'0');
+			}else {
+			//	throw new A(s);
 			}
 		}
 		
@@ -15,7 +17,11 @@ public class text {
 	}
 	
 	public static void main(String[] args) {
-		
+		try {
+			System.out.print(text.P("12345a"));
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 
 }
