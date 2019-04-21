a859dbe843b0bc2b2913e5cb53512af742ebea21
Eumenides997 <646224348@qq.com>
2019-04-21 17:17:34 +0800

    异常处理习题 张祯强 3171104131 第三次上传

--- src/异常处理习题/text.java
+++ src/异常处理习题/text.java
@@ -1,24 +1,39 @@
 package 异常处理习题;
 
-public class text {
-	
-	public static int P(String s) {
+class A extends Exception{
+	public A(String s) {
+		super(s);
+	}
+	public String getMessage() {
+		return String.format("\"%s\"",Thread.currentThread().getName())
+				+"For input string:"+super.getMessage();
+	}
+}
+
+
+class B{
+public static int P(String s) throws A {
 		
 		int f=0;
 		for(int i=0;i<s.length();i++) {
 			if(s.charAt(i)>='0'&&s.charAt(i)<='9') {
 				f=f*10+(s.charAt(i)-'0');
 			}else {
-			//	throw new A(s);
+				throw new A(s);
 			}
 		}
 		
 		return f;		
 	}
+}
+
+public class text {
+	
+	
 	
 	public static void main(String[] args) {
 		try {
-			System.out.print(text.P("12345a"));
+			System.out.print(B.P("12345a"));
 		}catch(Exception e){
 			e.printStackTrace();
 		}
